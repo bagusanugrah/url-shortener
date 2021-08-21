a29023c6bec2abf3bdaa85d5ccaf2ef99e83318b
Bagus Anugrah <bagus.anugrah71@gmail.com>
2021-08-22 06:37:22 +0700

    perubahan konfigurasi nodemailer

--- utils/nodemailer.js
+++ utils/nodemailer.js
@@ -1,19 +1,25 @@
 const nodemailer = require('nodemailer');
 
+const isSecure = process.env.NODEMAILER_ISSECURE === 'true' ? true : false;
+
 const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: 465,
-  secure: true,
+  host: process.env.NODEMAILER_HOST,
+  port: process.env.NODEMAILER_PORT,
+  secure: isSecure, // jika true pake SSL, jika false pake TLS/STARTTLS
   auth: {
-    user: process.env.EMAIL,
-    pass: process.env.PASSWORD,
+    user: process.env.NODEMAILER_EMAIL,
+    pass: process.env.NODEMAILER_PASSWORD,
+  },
+  tls: {
+    // do not fail on invalid certs
+    rejectUnauthorized: false,
   },
 });
 
 const sendEmailVerificationLink = (req, email, token) => {
   return transporter.sendMail({// kirimkan token yang ketemu tadi
     to: email,
-    from: `${req.domain}`,
+    from: process.env.NODEMAILER_EMAIL,
     subject: 'Verifikasi Email',
     html: `
         <h3>Klik link di bawah ini untuk verifikasi email anda</h3>
@@ -27,7 +33,7 @@ const sendEmailVerificationLink = (req, email, token) => {
 const sendResetPasswordLink = (req, email, token) => {
   return transporter.sendMail({
     to: email,
-    from: `${req.domain}`,
+    from: process.env.NODEMAILER_EMAIL,
     subject: 'Reset Password',
     html: `
         <h3>Klik link di bawah ini untuk ganti password anda</h3>
