67795cefe442e856f73814a134105085b312e118
Bagus Anugrah <bagus.anugrah71@gmail.com>
2021-09-01 16:46:15 +0700

    ubah doa dan ngasih jarak navbar dengan translator

--- public/js/translator.js
+++ public/js/translator.js
@@ -5,3 +5,17 @@ function translate() {
   var translator = document.getElementById('translator');
   translator.className = 'mt-2';
 }
+
+function addMarginTop() {
+  var layoutDefault = document.getElementById('layoutDefault');
+  var navContainer = document.getElementById('nav-container');
+
+  if (document.getElementsByClassName('goog-te-banner-frame').length !== 0){
+    layoutDefault.className = 'mt-5';
+    navContainer.className += ' mt-5';
+  }
+}
+
+function addToConsole(){
+  console.log(document.getElementsByClassName('goog-te-combo')[0].addEventListener('change', addMarginTop));
+}
