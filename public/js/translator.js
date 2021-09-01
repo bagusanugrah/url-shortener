fcecdd5adc3eeafdba667b0a0b6ac4a601fde074
Bagus Anugrah <bagus.anugrah71@gmail.com>
2021-09-01 10:38:18 +0700

    ngasih google translate

---
+++ public/js/translator.js
@@ -0,0 +1,7 @@
+var translatorBtn = document.getElementById('translate');
+translatorBtn.addEventListener('click', translate);
+
+function translate() {
+  var translator = document.getElementById('translator');
+  translator.className = 'mt-2';
+}
